00119989a6d0e861acc6bb93464b8b1b64e0a808
Anatoly Sinelnikov <noizywarf@gmail.com>
2025-09-04 23:33:45 +0300

    feat: upd first_lesson. Add error button

--- cpp/qt5_progs/first_lesson/main.cpp
+++ cpp/qt5_progs/first_lesson/main.cpp
@@ -1,13 +1,19 @@
 #include "QWidget"
 #include "QApplication"
 #include "QPushButton"
+#include "QMessageBox"
+#include "string"
 
 #include <QApplication>
 
 void onNock();
 QPushButton* btn_nock = nullptr;
-int size_count = 0, size_multiply = 55, max_count = 4;
+int size_count = 0, size_multiply = 50, max_count = 4;
 int  start_height = 50, start_weight = 200;
+
+void onError();
+QPushButton* btn_error = nullptr;
+char* error_message = "I said don't click!";
 
 int main(int argc, char *argv[])
 {
@@ -18,10 +24,14 @@ int main(int argc, char *argv[])
     widget.setMinimumSize(480, 360);
 
     btn_nock = new QPushButton("NOCK me", &widget);
-    btn_nock->setGeometry(QRect(QPoint(100, 100),QSize(start_weight, start_height)));
+    btn_nock->setGeometry(QRect(QPoint(50, 100),QSize(start_weight, start_height)));
     btn_nock->setMinimumSize(start_weight, start_height);
     btn_nock->setMaximumSize(start_weight, start_height);
     QObject::connect(btn_nock, &QPushButton::clicked, onNock);
+
+    btn_error = new QPushButton("Don't click", &widget);
+    btn_error->setGeometry(QRect(QPoint(10, 10),QSize(90, 30)));
+    QObject::connect(btn_error, &QPushButton::clicked, onError);
 
     widget.show();
     return app.exec();
@@ -42,4 +52,12 @@ void onNock()
         btn_nock->setMinimumSize(start_weight, start_height);
         btn_nock->setMaximumSize(start_weight, start_height);
     }
+}
+
+//
+void onError()
+{
+    QMessageBox msg;
+    msg.setText(error_message);
+    msg.exec();
 }
